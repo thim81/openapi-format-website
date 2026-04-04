ffd2f875e560c0d80b5dfd6dacad312ed87e9354
Tim Haselaars <timhaselaars@gmail.com>
2026-04-04 22:50:12 +0200

    chore: Remove @astrojs/tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://openapi-format.vercel.app',
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), sitemap()],
   server: {
     port: 7070,
   },
