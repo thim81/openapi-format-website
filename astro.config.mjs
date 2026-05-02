8744b24b99050fbf2c3de78303581455322913e5
Tim Haselaars <timhaselaars@gmail.com>
2026-05-02 14:04:31 +0200

    chore: Update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://openapi-format.vercel.app',
+  site: 'https://openapi-format.com',
   integrations: [react(), sitemap()],
   server: {
     port: 7070,
